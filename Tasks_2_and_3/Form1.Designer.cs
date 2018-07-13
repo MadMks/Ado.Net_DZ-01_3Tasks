8852ad0797f870eabdf69b4f053cca3ba6d754a7
T500 <mks59k@gmail.com>
2018-07-13 22:01:37 +0300

    Working tasks 2 and 3.

---
+++ Tasks_2_and_3/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Tasks_2_and_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCreateTable = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonCreateTable
+            // 
+            this.buttonCreateTable.Location = new System.Drawing.Point(32, 27);
+            this.buttonCreateTable.Name = "buttonCreateTable";
+            this.buttonCreateTable.Size = new System.Drawing.Size(288, 105);
+            this.buttonCreateTable.TabIndex = 0;
+            this.buttonCreateTable.Text = "Создать таблицу gruppa";
+            this.buttonCreateTable.UseVisualStyleBackColor = true;
+            this.buttonCreateTable.Click += new System.EventHandler(this.buttonCreateTable_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 165);
+            this.Controls.Add(this.buttonCreateTable);
+            this.Name = "Form1";
+            this.Text = "Task 2 and 3";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCreateTable;
+    }
+}
+
---
+++ Tasks_2_and_3/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tasks_2_and_3
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.connection = new SqlConnection();
+
+            SqlConnectionStringBuilder connectionStringBuilder = new SqlConnectionStringBuilder();
+            connectionStringBuilder.DataSource = @"T5";
+            connectionStringBuilder.InitialCatalog = "MyDB";
+            connectionStringBuilder.UserID = "loginMyDBDz01";
+            connectionStringBuilder.Password = "passwordMyDBDz01";
+
+            this.connection.ConnectionString = connectionStringBuilder.ConnectionString;
+        }
+
+        private void buttonCreateTable_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.connection?.Open();
+
+                CreateTableInMyDB();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+            finally
+            {
+                this.connection?.Close();
+            }
+        }
+
+        private void CreateTableInMyDB()
+        {
+            string insertString = @"
+                CREATE TABLE gruppa
+                (
+                    ID TINYINT NOT NULL PRIMARY KEY IDENTITY,
+                    Name NVARCHAR(15) NOT NULL
+                )";
+
+            SqlCommand command = new SqlCommand();
+            command.Connection = this.connection;
+            command.CommandText = insertString;
+
+            command.ExecuteNonQuery();
+        }
+    }
+}
